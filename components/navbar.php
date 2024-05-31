e193ef00c38196ec84b4263252734c98b9e81b6f
marcomit <marcomenegazzi.05@gmail.com>
2024-05-31 22:34:23 +0200

    preavviso email

--- components/navbar.php
+++ components/navbar.php
@@ -3,14 +3,17 @@ $items = [
     [
         "name" => "Dashboard",
         "icon" => "fas fa-fw fa-tachometer-alt",
+        "require_admin" => false
     ],
     [
         "name" => "Utenti",
-        "icon" => "fa fa-user"
+        "icon" => "fa fa-user",
+        "require_admin" => true
     ],
     [
         "name" => "Autorizzazioni",
         "icon" => "fas fa-fw fa-tachometer-alt",
+        "require_admin" => false,
         "sub_items" => [
             ["name" => "Autorizzazioni"],
             ["name" => "Richieste"],
@@ -18,15 +21,18 @@ $items = [
     ],
     [
         "name" => "Personale",
-        "icon" => "fa fa-id-badge"
+        "icon" => "fa fa-id-badge",
+        "require_admin" => false
     ],
     [
         "name" => "Veicoli",
-        "icon" => "fa fa-car"
+        "icon" => "fa fa-car",
+        "require_admin" => false
     ],
     [
         "name" => "Accessi",
-        "icon" => "fa fa-car"
+        "icon" => "fa fa-car",
+        "require_admin" => false
     ],
 ];
 
@@ -48,30 +54,31 @@ $current_idx = 0
     <hr class="sidebar-divider my-0">
 
     <?php for($item = 0; $item < count($items); $item++): ?>
-
-        <?php if(isset($items[$item]["sub_items"])): ?>
-            <li class="nav-item <?= $page_id >= $current_idx && $page_id <= $current_idx - 1 + count($items[$item]['sub_items']) ? "active" : "" ?>">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapse<?= $current_idx ?>"
-                    aria-expanded="true" aria-controls="collapse<?= $current_idx ?>">
-                    <i class="<?= $items[$item]["icon"] ?>"></i>
-                    <span><?= $items[$item]["name"] ?></span>
-                </a>
-                <div id="collapse<?= $current_idx ?>" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <?php foreach($items[$item]["sub_items"] as $sub_item): $current_idx++?>
-                            <a class="collapse-item" href="index.php?page_id=<?= $current_idx - 1 ?>"><?= $sub_item["name"] ?></a>
-                        <?php endforeach; ?>
+        <?php  if (($items[$item]['require_admin'] && $_SESSION['user']['RUOLO'] == "ADMIN") || !$items[$item]["require_admin"]): ?>
+            <?php if(isset($items[$item]["sub_items"])): ?>
+                <li class="nav-item <?= $page_id >= $current_idx && $page_id <= $current_idx - 1 + count($items[$item]['sub_items']) ? "active" : "" ?>">
+                    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapse<?= $current_idx ?>"
+                        aria-expanded="true" aria-controls="collapse<?= $current_idx ?>">
+                        <i class="<?= $items[$item]["icon"] ?>"></i>
+                        <span><?= $items[$item]["name"] ?></span>
+                    </a>
+                    <div id="collapse<?= $current_idx ?>" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                        <div class="bg-white py-2 collapse-inner rounded">
+                            <?php foreach($items[$item]["sub_items"] as $sub_item): $current_idx++?>
+                                <a class="collapse-item" href="index.php?page_id=<?= $current_idx - 1 ?>"><?= $sub_item["name"] ?></a>
+                            <?php endforeach; ?>
+                        </div>
                     </div>
-                </div>
-            </li>
-        <?php else: $current_idx++?>
-            <li class="nav-item <?= $page_id == $current_idx - 1 ? "active" : "" ?>">
-                <a class="nav-link" href="index.php?page_id=<?= $current_idx - 1 ?>">
-                    <i class="<?= $items[$item]["icon"] ?>"></i>
-                    <span><?= $items[$item]["name"] ?></span>
-                </a>
-            </li>
-        <?php endif; ?>
+                </li>
+            <?php else: $current_idx++?>
+                <li class="nav-item <?= $page_id == $current_idx - 1 ? "active" : "" ?>">
+                    <a class="nav-link" href="index.php?page_id=<?= $current_idx - 1 ?>">
+                        <i class="<?= $items[$item]["icon"] ?>"></i>
+                        <span><?= $items[$item]["name"] ?></span>
+                    </a>
+                </li>
+            <?php endif; ?>
+        <?php else: $current_idx++; endif; ?>
     <?php endfor; ?>
 
     <!-- Divider -->

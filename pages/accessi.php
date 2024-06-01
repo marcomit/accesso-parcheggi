bd77eed485dd68415488a8cf4b2e5624808290fd
SecondoCapo <100679260+SecondoCapo@users.noreply.github.com>
2024-06-01 10:29:57 +0200

    Accessi

--- pages/accessi.php
+++ pages/accessi.php
@@ -9,8 +9,9 @@ require_once(__DIR__ . '/../database.php');
 $db = new Database();
 if($_SESSION['user']['RUOLO']==="ADMIN"){
     $accessi = $db->execute_query(
-        "SELECT VEICOLI.TARGA AS Targa, VEICOLI.MODELLO AS Modello, ACCESSI_VEICOLO.ENTRATA AS Entrata, ACCESSI_VEICOLO.USCITA AS Uscita FROM VEICOLI
-        INNER JOIN ACCESSI_VEICOLO ON ACCESSI_VEICOLO.ID_VEICOLO = VEICOLI.ID");
+        "SELECT VEICOLI.TARGA AS Targa, VEICOLI.MODELLO AS Modello, ACCESSI_VEICOLO.ENTRATA AS Entrata, ACCESSI_VEICOLO.USCITA AS Uscita, UTENTI.NOME AS Nome, UTENTI.COGNOME AS Cognome FROM VEICOLI
+        INNER JOIN ACCESSI_VEICOLO ON ACCESSI_VEICOLO.ID_VEICOLO = VEICOLI.ID
+        INNER JOIN UTENTI ON UTENTI.ID=VEICOLI.ID_UTENTE");
 }
 else{
     $Utente=$_SESSION['user']['ID'];
@@ -26,20 +27,39 @@ else{
 <table class="table table-bordered" id="dataTable">
     <thead>
         <tr>
+        <?php if($_SESSION['user']['RUOLO']==="ADMIN"):?>
             <th data-sort="Entrata">Entrata</th>
             <th data-sort="Uscita">Uscita</th>
             <th data-sort="Targa">Targa</th>
             <th data-sort="Modello">Modello</th>
+            <th data-sort="Nome">Nome</th>
+            <th data-sort="Cognome">Cognome</th>
+            <?php else:?>
+                <th data-sort="Entrata">Entrata</th>
+                <th data-sort="Uscita">Uscita</th>
+                <th data-sort="Targa">Targa</th>
+                <th data-sort="Modello">Modello</th>
+        <?php endif;?>
             
         </tr>
     </thead>
     <tbody>
         <?php while($accesso = $accessi->fetch_assoc()): ?>
             <tr>
-                <td><?= $accesso['Entrata'] ?></td>
-                <td><?= $accesso['Uscita'] ?></td>
-                <td><?= $accesso['Targa'] ?></td>
-                <td><?= $accesso['Modello'] ?></td>
+                <?php if($_SESSION['user']['RUOLO']==="ADMIN"):?>
+                        <td><?= $accesso['Entrata'] ?></td>
+                        <td><?= $accesso['Uscita'] ?></td>
+                        <td><?= $accesso['Targa'] ?></td>
+                        <td><?= $accesso['Modello'] ?></td>
+                        <td><?= $accesso['Nome'] ?></td>
+                        <td><?= $accesso['Cognome'] ?></td>
+                        <?php else:?>
+                        <td><?= $accesso['Entrata'] ?></td>
+                        <td><?= $accesso['Uscita'] ?></td>
+                        <td><?= $accesso['Targa'] ?></td>
+                        <td><?= $accesso['Modello'] ?></td>
+                <?php endif;?>
+                
             </tr>
         <?php endwhile; ?>
     </tbody>
